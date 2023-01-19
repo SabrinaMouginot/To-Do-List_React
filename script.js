7abe9d3bc2e6ff267e55a827d1a4197d0ab8cb31
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-01-19 01:23:11 +0100

    ajout de style

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.write("<h1>To-do-list2</h1>");
-document.write("<form ref='form' onSubmit={this.onSubmit} className='form-inline'><input type='text' ref='itemName' className='form-control' placeholder='ajouter une nouvelle tâche...'/><button type='submit' className='btn btn-default'>Ajouter</button> </form>");
+document.write("<form ref='form' onSubmit={this.onSubmit} className='form-inline'><input class='area' type='text' ref='itemName' className='form-control' placeholder='ajouter une tâche...'/><button class='btn' type='submit' className='btn btn-default'>Ajouter</button> </form>");
 
