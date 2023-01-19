741e958213ce805fe8c0dc4a2c067ad9b810954a
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-01-19 01:08:27 +0100

    application du bootstrap et PaperCSS

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 document.write("<h1>To-do-list2</h1>");
 document.write("<form ref='form' onSubmit={this.onSubmit} className='form-inline'><input type='text' ref='itemName' className='form-control' placeholder='ajouter une nouvelle tâche...'/><button type='submit' className='btn btn-default'>Ajouter</button> </form>");
+
